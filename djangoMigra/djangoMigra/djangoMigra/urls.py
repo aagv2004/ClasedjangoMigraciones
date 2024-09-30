753c7cbe479f8291b77e413a0d26a634a3243a0e
Alejandro González <andresg.vergara04@gmail.com>
2024-09-30 12:37:37 -0300

    Se logró migrar
    aksdjisrh

--- djangoMigra/djangoMigra/djangoMigra/urls.py
+++ djangoMigra/djangoMigra/djangoMigra/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djangomigrApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.employeeData)
 ]
--- djangoMigra/djangoMigra/djangomigrApp/views.py
+++ djangoMigra/djangoMigra/djangomigrApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from djangomigrApp.models import Employee
 
 # Create your views here.
+def employeeData(request):
+    empleados = Employee.objects.all()
+    data = {'empleados': empleados}
+    return render(request, 'empleados.html', data)
